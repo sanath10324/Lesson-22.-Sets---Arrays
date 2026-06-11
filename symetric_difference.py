88d002334d8b056b9bba7d78165c4a8d894544fa
sanath10324 <sanathkatkamgamer@gmail.com>
2026-06-11 12:11:05 +0400

    Symetric Difference

---
+++ symetric_difference.py
@@ -0,0 +1,7 @@
+seta = {1, 2, 4, 7, 8}
+setb = {1, 3, 4, 8, }
+print("THe original sets are:")
+print(seta)
+print(setb)
+setc = seta.symmetric_difference(setb)
+print("The symetric difference is:", setc)
